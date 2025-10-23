ca50e73bd5df44c078e2790d4f19861aa4ff7283
Fanathing <mmybonnie@gamil.com>
2025-10-23 16:59:35 +0900

    Feat: main.css 설계 초안 , login.js 로그인 요청시 정상 응답 확인 , login.html에서 input name 응답 데이터 구조에 맞게 이름 변경

--- front/public/js/login.js
+++ front/public/js/login.js
@@ -1,20 +1,27 @@
 const loginLocal = document.querySelector(".login-local");
 
-console.log(loginLocal);
-
-
 loginLocal.addEventListener("submit", async(e) => {
     e.preventDefault();
+
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+    if(email.length === 0 || password.length === 0) {
+        alert("이메일 또는 비밀번호를 입력해주시길 바랍니다.")
+        return
+    }
     try {
         // 인풋정보 담아서 body로 보내주기
-        const { data } = await axios.post(`http://192.168.0.191:4000/api/stores/1/map`, {
-            userEmail: e.target.userEmail.value,
-            userPw: e.target.userPw.value,
+        const { data } = await axios.post(`http://192.168.0.191:4000/auth/login`, {
+            email: email,
+            password: password
         });
         alert(data.message)
         // 성공하면 메인페이지
-        window.location.href = "http://192.168.0.191:4000/"
+        // window.location.href = "http://192.168.0.191:4000/"
     } catch (error) {
-        alert(error.response.data.message || "이메일 또는 비밀번호가 틀렸습니다."); 
+        console.log(error);
+        
+        alert("이메일 또는 비밀번호가 틀렸습니다."); 
     }
 })
