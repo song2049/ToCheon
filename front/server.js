b871f5b25c3f8c309f876f2e778f7a26587d32a4
imbhj <wnqudgus5565@gmail.com>
2025-10-25 18:23:00 +0900

    feat(store): 라우터 계층 구조 분리 및 store 디렉토리 추가

--- front/server.js
+++ front/server.js
@@ -1,27 +1,30 @@
 require("dotenv").config();
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
-const nunjucks = require("nunjucks")
+const nunjucks = require("nunjucks");
 const path = require("path");
 const viewRouter = require("./view/view.router");
 const authRouter = require("./auth/auth.router");
+const storeRouter = require("./store/store.route");
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false}));
+// ✅ 절대경로로 정적 파일 경로 등록
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.set("view engine", "html");
-nunjucks.configure("views", { express: app});
+nunjucks.configure("views", { express: app });
 
 app.get("/", (req, res) => {
-    res.render("index.html");
-})
+  res.render("index.html");
+});
 
+app.use(storeRouter);
 app.use(viewRouter);
 app.use(authRouter);
 
 app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 });
