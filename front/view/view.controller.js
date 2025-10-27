d356a45124ec2285c74a2e6c1644294b527e2925
Fanathing <mmybonnie@gamil.com>
2025-10-27 14:25:16 +0900

    Feat: index.html에서 검색시에 원하는 가게 정보만 띄움, 하지만 검색결과가 3행 미만일 때 카드 위치가 조금 안 맞는 현상 있음

--- front/view/view.controller.js
+++ front/view/view.controller.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 const path = require("path");
 
+// 검색요청하면 여기서 리다이렉트 하는데 이때 쿼리스트링 값으로 던져줌
 const postSearch = async(req, res) => {
-        const { stores } = req.body
-         res.redirect(`/?stores=${encodeURIComponent(stores)}`);
+    const { stores } = req.body
+    res.redirect(`/?stores=${encodeURIComponent(stores)}`);
 };
-        
 
 const getReview = (req, res) => {
     res.render("detail.html");
