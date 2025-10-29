4c4c27956966c7caaff7490b187af40e668f9baf
Fanathing <mmybonnie@gamil.com>
2025-10-29 15:53:09 +0900

    Feat: 어드민 페이지 페이지네이션 기능 구현 및 상단 대기목록 숫자들 동적으로 구현

--- admin/front/server.js
+++ admin/front/server.js
@@ -18,11 +18,40 @@ app.set("view engine", "html");
 nunjucks.configure("views", { express: app});
 
 app.get("/admin", async(req, res) => {
-    const { data } = await axios.get("http://localhost:4000/api/admin/stores/pending");
+    try {
+        // 페이지 네이션 정의
+        const stores = req.query.stores || "";
+        const page = parseInt(req.query.page) || 1;
+        const limit = 10;
+        const offset = (page - 1) * limit;
 
-    console.log(data.items);
-    
-    res.render("index.html")
+        // 승인 대기
+        const { data: waiting } = await axios.get("http://localhost:4000/api/admin/stores/pending");
+        // 승인된 상점
+        const { data: approval } = await axios.get("http://localhost:4000/api/stores");
+        // 어드민 상단 갯수들
+        const waitingData = waiting.items
+        const waitingNum = waiting.items.length
+        const approvalNum = approval.length
+        // 페이지 네이션 작업
+        const totalItems = waitingData.length;
+        const totalPages = Math.ceil(totalItems / limit);
+        const pageData = waitingData.slice(offset, offset + limit);
+        const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+        res.render("index.html",{
+            stores,
+            waitingData,
+            waitingNum,
+            approvalNum,
+            pageData,
+            pages,
+            page
+        })
+    } catch (error) {
+        res.render("index.html",{
+            stores: []
+        })
+    }
 })
 
 app.listen(PORT, () => {
